7684468d7fbf88690522bbe4823c1f75e120e146
asavershin <avershinrussian@gmail.com>
2024-05-08 14:26:13 +0300

    Remove insert default devices

--- java/DeviceStorage/db-init/init.sql
+++ java/DeviceStorage/db-init/init.sql
@@ -3,7 +3,3 @@ CREATE TABLE device (
     device_password VARCHAR(255) NOT NULL,
     PRIMARY KEY (device_name, device_password)
 );
-
-INSERT INTO device (device_name, device_password)
-VALUES ('device1', 'VLf5DbMKoYYb+pualiE9CA=='),
-       ('device2', 'NB0FJ/t8ePXnIqERpm3d7w==');
